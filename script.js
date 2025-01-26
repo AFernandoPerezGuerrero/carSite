eff8a2896341728704721424dc3012a28ea80618
AFernandoPerezGuerrero <andresperez.contact@gmail.com>
2025-01-26 00:54:33 -0500

    Page layeout completed, interaction and responsiveness remaining to code

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const body = document.querySelector("body"),
    sidebar = body.querySelector(".sidebar"),
    toggle = body.querySelector(".toggle"),
+   openArrow = body.querySelector(".open-arrow"),
    modeSwitch = body.querySelector(".toggle-switch"),
    navButton = body.querySelector("nav-text"),
+   catalogButton = document.getElementById("autos-button"),
    modeText = body.querySelector(".mode-text");
 
 
    toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+   })
+
+   openArrow.addEventListener("click", () => {
     sidebar.classList.toggle("close");
    })
 
@@ -25,4 +31,13 @@ const body = document.querySelector("body"),
 
    })
 
+
+   catalogButton.addEventListener('click', () => {
+    const autosSection = document.querySelector('.autos-catalog');  // replace with the actual class or ID
+    autosSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+       
+    });
+});
 
